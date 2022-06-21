ca3aab0a94d4c5298cf5feabfaf48a13e6d30c08
Daniel Colares <danielmcolares@gmail.com>
2022-06-21 11:56:02 -0300

    Aula 01 - Abertura Concluida

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
+import { gql, useQuery } from "@apollo/client"
+import { useEffect } from "react"
+import { client } from "./lib/apollo"
+const GET_LESSONS_QUERY = gql`
+query{
+  lessons{
+    id
+    title
+  }
+}
+`
 function App() {
 
+  const { data } = useQuery(GET_LESSONS_QUERY)
+  console.log(data);
+  
+  /*
+  useEffect(()=>{
+    client.query({
+      query: GET_LESSONS_QUERY,
+    }).then (response =>{
+      console.log(response.data);
+    })
+  },[])*/
+
+
   return (
-    <div className="App">
-    </div>
+    <h1 className="text-5xl font-bold text-violet-400">Hello  World</h1>
   )
 }
 
